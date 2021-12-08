9d1e7c72fa75d424cb5effada00b25075f591d5e
Gabriel Oliveira <gabrieljoliveira@gmail.com>
2021-12-08 17:12:43 -0300

    fix test view func

--- app/src/androidTest/java/com/example/cep_koin/ui/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/cep_koin/ui/MainActivityTest.kt
@@ -1,11 +1,11 @@
 package com.example.cep_koin.ui
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.hasErrorText
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.cep_koin.R
@@ -26,8 +26,10 @@ class MainActivityTest {
     }
 
     @Test
-    fun when_field_is_not_empty_should_disable_button_then_show_progress_bar() {
+    fun when_field_is_not_empty_should_disable_button_then_hide_keyboard_and_show_progress_bar() {
         onView(withId(R.id.cep_edit_text)).perform(typeText("06240000"))
         onView(withId(R.id.button)).perform(click())
+        onView(withId(R.id.button)).check(matches(isClickable()))
+        onView(withId(R.id.progress_cep_loading)).check(matches(isDisplayed()))
     }
 }
