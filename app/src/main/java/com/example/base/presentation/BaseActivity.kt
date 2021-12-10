d2edc01bf687e26e76e13ebcf80daeecce609ce6
Gabriel Oliveira <gabrieljoliveira@gmail.com>
2021-12-10 11:09:49 -0300

    created new fun to show a short toast

--- app/src/main/java/com/example/base/presentation/BaseActivity.kt
+++ app/src/main/java/com/example/base/presentation/BaseActivity.kt
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 
 open class BaseActivity: AppCompatActivity() {
 
-    fun showToast(msg: String) {
+    fun showToastLong(msg: String) {
         Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+    }
+
+    fun showToastShort(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 }
